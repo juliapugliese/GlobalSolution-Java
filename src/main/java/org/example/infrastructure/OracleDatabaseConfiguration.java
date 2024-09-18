8e0576806393b698aa24048f1cbe0e0b72e9149b
Julia Pugliese <julia03.pugliese@gmail.com>
2024-09-18 08:13:56 -0300

    Update OracleDatabaseConfiguration.java

--- src/main/java/org/example/infrastructure/OracleDatabaseConfiguration.java
+++ src/main/java/org/example/infrastructure/OracleDatabaseConfiguration.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class OracleDatabaseConfiguration {
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-    private static final String USER = "rm553427";
-    private static final String PASSWORD = "280603";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
     public Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
